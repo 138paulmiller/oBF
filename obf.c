77d0cb4ab2f79eefc7dad33b4100ba9d359fa14d
138paulmiller@gmail.com <138paulmiller@gmail.com>
2017-11-10 09:52:23 -0800

    Termination bug fix

--- obf.c
+++ obf.c
@@ -4,7 +4,6 @@
 #define G(x,y,z,w)*I,*T,i,j,k,c,**V,v=1,d;F(H,x)F(E,y)F(L,z)F(P,w)(*A[])()={H,E,L,P};
 G({a?--k:v<d?(k[T]= atoi(V[v++])):exit(0);},{a?(k[T]?go(-1):0):--k[T];},{a?++k:Z(pu,k[T]);},{a?(k[T]?0:go(1)):++k[T];})
 inline Q(c,z,e){z=c/10;e=c%10;return ~c?((z^4||e^3&&e^4&&e^5&&e^6)?(z^6||e^0&&e^2)?(z^9||e^1&&e^3)?0:c:c:c)&&(I[++i]=c):i++,c;}
-inline go(d){c=d;while((I[(i+=d)]-91)?(I[i]-93)?1:(c-=1):(c+=1));}main(a,_){V=_,j=i,i=-1,k=0;a?(I=sbrk(SZ)):(T=sbrk(SZ));
-while(a?~Q(Z(ge,))||main((d=a)*0,_):++i<j&&1+(A[(I[i]&0xF)%4]((I[i]>>4)-2)));}
-
-
+inline go(d){c=d;while((I[(i+=d)]-91)?(I[i]-93)?1:(c-=1):(c+=1));}
+main(a,_){V=_,j=i,i=-1,k=0;a?(I=sbrk(SZ)):(T=sbrk(SZ));while(a?~Q(Z(ge,))||
+main((d=a)*0,_):++i<=j&&1|(A[(I[i]&0xF)%4]((I[i]>>4)-2)));}
